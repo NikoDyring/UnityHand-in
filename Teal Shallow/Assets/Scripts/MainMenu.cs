0ce2b6f4a9c836bce45d8fb2a7b94a1b8413972a
Nikolaj Dyring <nikodyring@gmail.com>
2019-12-27 23:55:55 +0100

    Scrapped project due to bugs with VSCode. Started generating funky code.

---
+++ Teal Shallow/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using  UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject optionsWindow;
+    [SerializeField]
+    private GameObject mainWindow;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level_01");
+    }
+
+    public void OpenOptions()
+    {
+        mainWindow.SetActive(false);
+        optionsWindow.SetActive(true);
+    }
+
+    public void SaveOptions()
+    {
+        // TODO: Implement Playerprefs here.
+        optionsWindow.SetActive(false);
+        mainWindow.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Teal Shallow/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float moveSpeed = 5f;
+    public Rigidbody2D playerRB;
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        FaceMouse();
+    }
+
+    void FixedUpdate()
+    {
+        playerRB.MovePosition(playerRB.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    void FaceMouse()
+    {
+             Vector3 mousePos = Input.mousePosition;
+             mousePos.z = 5.23f;
+     
+             Vector3 objectPos = Camera.main.WorldToScreenPoint (transform.position);
+             mousePos.x = mousePos.x - objectPos.x;
+             mousePos.y = mousePos.y - objectPos.y;
+     
+             float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+             transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+}
